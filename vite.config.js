a51c1ddf355be599e8e431567be2132b7f7f147a
Mitchell <a1@Mitchells-MacBook-Air.local>
2025-02-07 04:34:46 -0600

    Test vite config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,8 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   base: process.env.NODE_ENV === 'production' ? '/reactMitchell/' : '/',
-// });
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',  // Use './' to make relative paths work
+  base: process.env.NODE_ENV === 'production' ? '/reactMitchell/' : '/',
 });
