b6dee8b314890bae334210b86cce5e1e3866761c
Mitchell <a1@Mitchells-MacBook-Air.local>
2025-02-18 03:09:22 -0600

    Add photo slider feature

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "../style/Body.css"; 
 import VideoCarousel from "./VideoCarousel"; 
+import PhotoSlider from "./PhotoSlider"; 
 import { characters } from "../utils/languagechars"; 
 
 const Body = ({ isDarkMode }) => {
@@ -42,8 +43,33 @@ const Body = ({ isDarkMode }) => {
       ))}
 
       {/* Video Carousel */}
-      <div className="video-carousel-container">
+      {/* <div className="video-carousel-container">
         <VideoCarousel />
+      </div> */}
+      <div className="photo-slider-container">
+        <PhotoSlider 
+          images={[
+            "/assets/images/iceland.png",
+            "/assets/images/thailand.png",
+            "/assets/images/angelfall.png", 
+            "/assets/images/fjord.png",
+            "/assets/images/grandCentral.png",
+            "/assets/images/haivanpass.JPG",
+            "/assets/images/hanoi.JPG",
+            "/assets/images/hongKong.png",
+            "/assets/images/japan.png",
+            "/assets/images/MV11.jpg",
+            "/assets/images/myanmar.png",
+            "/assets/images/navajo.png",
+            "/assets/images/norway.png",
+            "/assets/images/pagoda.png",
+            "/assets/images/sappo.png",
+            "/assets/images/train.png",
+            "/assets/images/vietnam.JPG",
+            "/assets/images/vietnam.png",
+            "/assets/images/waterfall.jpg"
+          ]} 
+        />
       </div>
     </div>
   );
