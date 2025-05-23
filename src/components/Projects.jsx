5de9d841996bb69d3da1b1b98b2a2568b7fc7c85
Mitchell <a1@Mac.cable.rcn.com>
2025-05-22 21:01:46 -0500

    Add nav bar to header

---
+++ src/components/Projects.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../style/Projects.css"; 
+
+const Projects = () => {
+  return (
+    <div className="projects-container">
+        <div className="projects-content">
+            <h2>Projects</h2>
+            <p>My recent software and engineering projects are coming soon.</p>
+            {/* Add content here */}
+        </div>
+    </div>
+  );
+};
+
+export default Projects;
