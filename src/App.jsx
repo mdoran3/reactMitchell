af1944c06cc01f692ed22acfad5d728712377d07
Mitchell <mitchelld.dev@gmail.com>
2026-08-02 23:49:08 -0500

    Rename song "Outrun" to "Run"

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
   const [currentTab, setCurrentTab] = useState('travel');
   const [currentSong, setCurrentSong] = useState({
     url: "https://pub-5c6372312189426f903f701c7e1544e5.r2.dev/Run%20-%20Wet%20Stem%20Mix%20(3%3A27).wav",
-    name: "Outrun (original mix)",
+    name: "Run (original mix)",
   });
   const [isPlaying, setIsPlaying] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
--- src/components/Audio.jsx
+++ src/components/Audio.jsx
@@ -6,7 +6,7 @@ import { Typewriter } from "react-simple-typewriter";
 const Audio = ({ setCurrentSong, isDarkMode, currentSong, isPlaying, isLoading }) => {
   const songs = [
     {
-      name: "Outrun (original mix)",
+      name: "Run (original mix)",
       url: "https://pub-5c6372312189426f903f701c7e1544e5.r2.dev/Run%20-%20Wet%20Stem%20Mix%20(3%3A27).wav",
     },
     {
